5721752e237061eb5def01720e8725223e99277d
Griffin Milsap <griffin.milsap@jhuapl.edu>
2025-02-13 17:17:24 -0500

    simplification and removal of intermodulation

--- src/ezmsg/tasks/ssvep/task.py
+++ src/ezmsg/tasks/ssvep/task.py
@@ -1,6 +1,3 @@
-import math
-import time
-import param
 import typing
 import asyncio
 import datetime
@@ -22,10 +19,16 @@ from ..task import (
 )
 
 from .dynamicclasses import DynamicClasses
-from .stimulus import SSVEPStimulus, VisualMotionStimulus, IntermodulationSSVEP
+from .stimulus import CanvasStimulus, SSVEPStimulus, VisualMotionStimulus, SimpleSSVEP
 from ..frequencydecodemessage import FrequencyDecodeMessage
 
 STIMULUS_KWARGS = dict(width = 300, height = 200)
+
+STIMULUS_CLASSES: typing.Dict[str, typing.Type[CanvasStimulus]] = {
+    'Checkerboard': SSVEPStimulus,
+    'Visual Motion': VisualMotionStimulus,
+    'Simple': SimpleSSVEP
+}
 
 @dataclass
 class SSVEPSampleTriggerMessage(SampleTriggerMessage):
@@ -40,9 +43,8 @@ class SSVEPTaskImplementationState(TaskImplementationState):
     stimulus: pn.layout.Row
     task_area: pn.layout.Card
 
-    classes: pn.Column #DynamicClasses
+    classes: DynamicClasses
     multiclass: pn.widgets.Checkbox
-    #rotation: pn.widgets.Checkbox
     stimulus_type: pn.widgets.Select
     stimulus_size: pn.widgets.IntInput
 
@@ -76,8 +78,10 @@ class SSVEPTaskImplementation(TaskImplementation):
     
     async def initialize(self) -> None:
         await super().initialize()
-        self.STATE.stimulus = pn.layout.Row(sizing_mode = 'stretch_width')
 
+        sw = dict(sizing_mode = 'stretch_width')
+
+        self.STATE.stimulus = pn.layout.Row(**sw)
         self.STATE.task_area = pn.Card(
             pn.Column(
                 pn.layout.VSpacer(),
@@ -90,13 +94,12 @@ class SSVEPTaskImplementation(TaskImplementation):
             sizing_mode = 'stretch_both'
         )
 
-        sw = dict(sizing_mode = 'stretch_width')
-        self.STATE.classes = DynamicClasses(intermod=False)#, n_classes=param.Integer(default=0))
-        self.STATE.multiclass = pn.widgets.Checkbox(name = 'Multiclass Presentation', value = False, **sw)
-        self.STATE.stimulus_type = pn.widgets.Select(name='Stimulus Type', options=['Checkerboard', 'Visual Motion', 'Intermodulation'])
+        self.STATE.classes = DynamicClasses(**sw)
+        self.STATE.multiclass = pn.widgets.Checkbox(name = 'Multiclass Presentation', value = True, **sw)
+        self.STATE.stimulus_type = pn.widgets.Select(name = 'Stimulus Type', options = list(STIMULUS_CLASSES.keys()))
         self.STATE.stimulus_size = pn.widgets.IntInput(name = 'Stimulus Size (pixels)', value = 200, start = 10, **sw)
 
-        self.STATE.trials_per_class = pn.widgets.IntInput(name = 'Trials per-class', value = 10, start = 1, **sw)
+        self.STATE.trials_per_class = pn.widgets.IntInput(name = 'Trials per-class', value = 2, start = 1, **sw)
         self.STATE.feedback = pn.widgets.Checkbox(name = 'Display Feedback', value = False, **sw)
         self.STATE.pre_run_duration = pn.widgets.FloatInput(name = 'Pre-run (sec)', value = 3, start = 0, **sw)
         self.STATE.post_run_duration = pn.widgets.FloatInput(name = 'Post-run (sec)', value = 3, start = 0, **sw)
@@ -104,8 +107,6 @@ class SSVEPTaskImplementation(TaskImplementation):
         self.STATE.trial_duration = pn.widgets.FloatInput(name = 'Trial dur. (sec)', value = 4.0, step = 0.1, start = 0.1, end = self.SETTINGS.buffer_dur, **sw)
         self.STATE.intertrial_min_dur = pn.widgets.FloatInput(name = 'ITI Min (sec)', value = 1.0, start = 0, step = 0.1, **sw)
         self.STATE.intertrial_max_dur = pn.widgets.FloatInput(name = 'ITI Max (sec)', value = 2.0, start = self.STATE.intertrial_min_dur.param.value, step = 0.1, **sw)
-
-        
 
         @pn.depends(
                 self.STATE.classes.classes.param.params()['objects'],
@@ -133,18 +134,16 @@ class SSVEPTaskImplementation(TaskImplementation):
             run_len = pre_run + run_len + post_run
             run_dur = str(datetime.timedelta(seconds = run_len))
             self.STATE.run_info.value = f'{num_classes} class(es), {n_trials} trial(s), ~{run_dur}'
+
         # This is done here to kick the calculation for run_calc
-        self.STATE.trials_per_class.param.update(value = 20)
         self.STATE.trials_per_class.param.update(value = 10)
-        self.STATE.stimulus.clear()
 
         self.STATE.task_controls = pn.WidgetBox(
-            self.STATE.classes,
-            self.STATE.stimulus_size,
-            self.STATE.multiclass,
-            #self.STATE.rotation,
             self.STATE.stimulus_type,
+            self.STATE.classes,
+            self.STATE.multiclass,
             self.STATE.feedback,
+            self.STATE.stimulus_size,
             pn.Row(
                 self.STATE.trials_per_class,
                 self.STATE.trial_duration,
@@ -159,18 +158,6 @@ class SSVEPTaskImplementation(TaskImplementation):
             ),
             sizing_mode = 'stretch_both'
         )
-
-        ''' handles ui changes related to different parameters needed for intermodulation and checker/vm'''        
-        async def stim_type_callback(event):
-            self.STATE.task_controls.remove(self.STATE.classes)
-            if event.new == 'Intermodulation':
-                print('intermod true')
-                self.STATE.classes = DynamicClasses(intermod=True)
-            elif event.old == 'Intermodulation':
-                self.STATE.classes = DynamicClasses(intermod=False)
-            self.STATE.task_controls.insert(0, self.STATE.classes)
-
-        self.STATE.stimulus_type.param.watch(stim_type_callback, 'value')
         
         self.STATE.output_class = asyncio.Queue()
         self.STATE.input_decode = asyncio.Queue()
@@ -202,22 +189,29 @@ class SSVEPTaskImplementation(TaskImplementation):
             multiclass: bool = self.STATE.multiclass.value # type: ignore
             stimulus_type: str = self.STATE.stimulus_type.value # type: ignore
 
-            #print(self.STATE.classes)
-            #print(self.STATE.classes[0].setting)
-            classes = self.STATE.classes.setting
-            print(classes)
-            for name, freqs in self.STATE.classes.setting.items():
-                if(freqs[0] == 0):
-                    del classes[name]
-                elif(stimulus_type == 'Intermodulation' and freqs[1] == 0):
-                    del classes[name]
+            class_periods = self.STATE.classes.setting
+            class_indices = list(range(len(class_periods)))
+            class_stimuli = {
+                idx: STIMULUS_CLASSES[stimulus_type](
+                    period_ms = per, 
+                    width = stimulus_size, 
+                    height = stimulus_size,
+                    presented = False,
+                    border = 0,
+                ) for idx, per in zip(class_indices, class_periods)
+            }
 
+            self.STATE.stimulus.clear()
+            elements: typing.List[pn.viewable.Viewable] = [pn.layout.HSpacer()]
+            for stimulus in class_stimuli.values():
+                elements.extend([stimulus, pn.layout.HSpacer()])
+            self.STATE.stimulus.extend(elements)
 
             # Create trial order (blockwise randomized)
-            trials: typing.List[str] = []
+            trials: typing.List[int] = []
             for _ in range(trials_per_class):
-                random.shuffle(list(classes.keys()))
-                trials += classes
+                random.shuffle(class_indices)
+                trials += class_indices
 
             self.STATE.progress.max = len(trials)
             self.STATE.progress.value = 0
@@ -228,76 +222,34 @@ class SSVEPTaskImplementation(TaskImplementation):
             await asyncio.sleep(pre_run_duration)
             self.STATE.input_decode = asyncio.Queue() # clear out the decode queue
 
-            for trial_idx, trial_class in enumerate(trials):
+            for trial_idx, trial_class_idx in enumerate(trials):
                 trial_id = f'Trial {trial_idx + 1} / {len(trials)}'
                 
+                # Do inter-trial interval
                 self.STATE.status.value = f'{trial_id}: Intertrial Interval'
                 iti = random.uniform(iti_min, iti_max)
-                self.STATE.stimulus.clear()
                 self.STATE.output_class.put_nowait(None)
-                trial_str = pn.pane.Markdown("""### Starting {}""".format(trial_id), align=('center'))#pn.pane.Str(f'Starting {trial_id}')
-                self.STATE.task_area.insert(0, trial_str)
                 await asyncio.sleep(iti)
-                self.STATE.task_area.remove(trial_str)
 
-                stimuli = []
-                if(stimulus_type == 'Checkerboard'):
-                    stimuli = [SSVEPStimulus(
-                        period_ms = int(1000/c[0]), 
-                        width = stimulus_size, 
-                        height = stimulus_size,
-                        presented = False,
-                        border = 3 if f == trial_class else 0,
-                        ) for f, c in classes.items()
-                    ]
-                elif (stimulus_type == 'Visual Motion'):
-                    stimuli = [VisualMotionStimulus(
-                        period_ms = int(1000/c[0]), 
-                        width = stimulus_size, 
-                        height = stimulus_size,
-                        presented = False,
-                        border = 3 if f == trial_class else 0,
-                        ) for f, c in classes.items()
-                    ]
-                elif(stimulus_type == 'Intermodulation'):
-                    stimuli = [IntermodulationSSVEP(
-                            period_ms = int(1000/c[0]), 
-                            period_ms_2 = int(1000/c[1]),
-                            width = stimulus_size, 
-                            height = stimulus_size,
-                            presented = False,
-                            border = 3 if f == trial_class else 0,
-                            ) for f, c in classes.items()
-                        ] 
-
-
-                target_stim = next(s for s in stimuli if s.period_ms == int(1000/classes[trial_class][0]))
-
-                self.STATE.stimulus.append(pn.layout.HSpacer())
-                if multiclass:
-                    for stim in stimuli:
-                        self.STATE.stimulus.append(stim)
-                        self.STATE.stimulus.append(pn.layout.HSpacer())
-                else:
-                    self.STATE.stimulus.append(target_stim)
-                    self.STATE.stimulus.append(pn.layout.HSpacer())
-
+                # Present focus cue
+                class_stimuli[trial_class_idx].border = 3
                 await asyncio.sleep(1.0)
 
-                for stim in stimuli:
-                    stim.presented = True
+                # Present stimuli
+                for idx, stim in class_stimuli.items():
+                    stim.presented = True if multiclass else trial_class_idx == idx
 
-                self.STATE.status.value = f'{trial_id}: Action ({classes[trial_class][0]:.02f})'
-                self.STATE.output_class.put_nowait(trial_class)
+                self.STATE.status.value = f'{trial_id}: Action ({1000/class_periods[trial_class_idx]:.02f})'
+                self.STATE.output_class.put_nowait(str(trial_class_idx))
                 yield SSVEPSampleTriggerMessage(
                     period = (0.0, trial_dur), 
-                    value = trial_class,
-                    reversal_period_ms = [stim.period_ms for stim in stimuli], # type: ignore
-                    target = stimuli.index(target_stim)
+                    value = str(trial_class_idx),
+                    reversal_period_ms = class_periods,
+                    target = trial_class_idx
                 )
                 await asyncio.sleep(trial_dur)
 
-                for stim in stimuli:
+                for stim in class_stimuli.values():
                     stim.presented = False
                     stim.border = 0
 
@@ -309,16 +261,20 @@ class SSVEPTaskImplementation(TaskImplementation):
                         decode = await asyncio.wait_for(self.STATE.input_decode.get(), timeout = 2.0)
                         focus_idx = np.argmax(decode.data).item()
                         focus_per = round(1000.0 / decode.freqs[focus_idx])
-                        correct = focus_per == target_stim.period_ms
-                        ez.logger.info(f'{trial_class=}, {decode=}, {correct=}')
+                        correct = focus_per == class_periods[trial_class_idx]
+                        ez.logger.info(f'{trial_class_idx=}, {decode=}, {correct=}')
 
-                        for stim in stimuli:
+                        for stim in class_stimuli.values():
                             if stim.period_ms == focus_per:
                                 stim.border = 3
 
                         await asyncio.sleep(0.5)                   
                     except asyncio.TimeoutError:
                         ez.logger.info('Feedback requested, but no decode received')
+
+                    for stim in class_stimuli.values():
+                        stim.presented = False
+                        stim.border = 0
                 
                 self.STATE.progress.value = trial_idx + 1
 
